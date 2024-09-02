f0aa3d6115f3b74d839c21fbf2a105282c575f48
erick2507 <169068468+erick2507@users.noreply.github.com>
2024-09-02 12:38:36 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Série: Sobrenatural");
+    System.out.println("Em Sobrenatural é apresentado para o público dois personagens principais seus nomes são Sam e dean Winchester eles são ca"çadores de criaturas Sobrenaturais eles falam que fazem isso pelo legado da familia no começo eles só estão atrás de vingança contra um demônio que matou a mãe deles, depois que eles conseguem a vingança eles irão impedir o apocalipse e continuar a caçar criaturas seguindo o legado da família Winchester);
+ 
+  }
+
+  
+}
